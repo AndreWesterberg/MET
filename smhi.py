6997c61907ecf2569b83bb90c77dfb3dede66996
AndreWesterberg <andre.nikolaj.westerberg@gmail.com>
2023-01-18 13:01:14 +0100

    completed functions request_weather_data and raw_json

--- smhi.py
+++ smhi.py
@@ -4,24 +4,30 @@ import pandas as pd
 import os 
 import sqlalchemy
 import psycopg2
+import json
 
 
 # Variables and constants
 current_path = os.path.dirname(os.path.realpath(__file__))
 data_path = current_path + "//data" 
-url = None
+url = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/18/lat/59/data.json'
 raw_file = "//raw.json"
 harmonized_file = "//harmonized.json"
 cleansed_file = "//cleansed.json"
+
 
 # Functions
 
 def request_weather_data(url):
     r = requests.get(url)
-    
+    return r
 
-def raw_json(request_object, save_path):
-    pass
+def raw_json(response_object, save_path):
+    f = open(save_path, "w")
+    f.write(json.dumps(response_object.json()))
+    f.close()
+
+
 
 def raw_to_harmonized(load_path, save_path):
     pass
@@ -38,3 +44,5 @@ def clensed_to_db(load_path):
 
 
 # Main program
+response = request_weather_data(url)
+raw_json(response, data_path + raw_file)
